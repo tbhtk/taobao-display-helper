b8400467010f19f6b3233b0322c5e4708f6e9954
tbhtk <git@tbhtk.ru>
2017-11-01 05:48:36 +0800

    Added Sample Screenshot

---
+++ taobao-display-helper.user.js
@@ -0,0 +1,50 @@
+// ==UserScript==
+// @name                淘寶顯示助手【雙11版】
+// @name:en-US          Taobao Display Helper (1111)
+// @namespace           http://tbhtk.ru
+// @version             0.1
+// @license             MIT
+// @description         【淘寶搜尋頁】1. 雙11顯示「店鋪紅包」圖示 2. 顯示所有雙11圖示
+// @author              熊之淘寶谷
+// @homepage            http://tbhtk.ru
+
+// @match               https://s.taobao.com/search?*
+// @match               http://s.taobao.com/search?*
+
+// @run-at              document-start
+// @grant               GM_setValue
+// @grant               GM_getValue
+// @grant               GM_addStyle
+// ==/UserScript==
+
+/*=================
+ * Settings
+ *=================*/
+var is_show_shop_pocket = true; // 顯示「店鋪紅包」圖示
+var is_show_all_1111_labels = true; // 顯示所有雙11圖示
+
+/*=================
+ * Main
+ *=================*/
+(function() {
+  "use strict";
+
+  if (is_show_shop_pocket) {
+    GM_addStyle(
+      ".icon-fest-2017taobaodianpuhong { background: url(https://img.alicdn.com/tfs/TB1D5hoab_I8KJjy1XaXXbsxpXa-407-396.png);   background-repeat: no-repeat; display: inline-block; background-position: -147px -304px;  width: 75px;  height: 16px; }"
+    );
+  }
+  
+  if (is_show_all_1111_labels) {
+    // .item-ctx-hover
+    GM_addStyle(".m-itemlist .grid .item { height: 374px !important }");
+    GM_addStyle(
+      ".response-narrow  .m-itemlist .grid .item { height: 334px !important}"
+    );
+    GM_addStyle(
+      ".m-itemlist .grid .row-4 { margin-top: 1px !important; margin-bottom: 20px;}"
+    );
+    // GM_addStyle('.m-itemlist .grid .row-4, .m-itemlist .icon-has-more { overflow: visible !important}');
+    GM_addStyle(".m-itemlist .grid .icon { margin-top: 0px !important; }");
+  }
+})();
