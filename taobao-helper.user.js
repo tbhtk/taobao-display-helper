9d289cc32f354b9e5aa57403eaacfb59e95a0a2e
tbhtk <git@tbhtk.ru>
2017-11-05 02:46:10 +0800

    0.1.6

--- taobao-helper.user.js
+++ taobao-helper.user.js
@@ -1,10 +1,11 @@
 // ==UserScript==
 // @name                淘寶助手
-// @name:en-US          Taobao Helper
+// @name:en             Taobao Helper
 // @namespace           http://tbhtk.ru
-// @version             0.1
+// @version             0.1.6
 // @license             MIT
-// @description         【淘寶搜尋頁】1. Desktop版顯示雙11「店鋪紅包」圖示  2. 不需要Mouse Over顯示所有雙11圖示（店鋪紅包及滿減等）
+// @description         【淘寶搜尋頁】1. 提前顯示雙11「店鋪紅包」圖示  2. 以不同顏色顥示各種圖示 3. 不需要Mouse Over顯示所有雙11圖示（店鋪紅包及滿減等）
+// @description:en        Please check Chinese version
 // @icon                https://www.tbhtk.ru/static/favicon.png
 // @icon64              https://www.tbhtk.ru/static/favicon.png
 // @author              熊之淘寶谷
@@ -24,29 +25,39 @@
  *=================*/
 var is_show_shop_pocket = true; // 顯示「店鋪紅包」圖示
 var is_show_all_1111_labels = true; // 顯示所有雙11圖示
+var is_change_label_color = true; // 調整圖示顏色
 
 /*=================
  * Main
  *=================*/
 (function() {
-  "use strict";
+    "use strict";
 
-  if (is_show_shop_pocket) {
-    GM_addStyle(
-      ".icon-fest-2017taobaodianpuhong { background: url(https://img.alicdn.com/tfs/TB1D5hoab_I8KJjy1XaXXbsxpXa-407-396.png);   background-repeat: no-repeat; display: inline-block; background-position: -147px -304px;  width: 75px;  height: 16px; }"
-    );
-  }
-  
-  if (is_show_all_1111_labels) {
-    // .item-ctx-hover
-    GM_addStyle(".m-itemlist .grid .item { height: 374px !important }");
-    GM_addStyle(
-      ".response-narrow  .m-itemlist .grid .item { height: 334px !important}"
-    );
-    GM_addStyle(
-      ".m-itemlist .grid .row-4 { margin-top: 1px !important; margin-bottom: 20px;}"
-    );
-    // GM_addStyle('.m-itemlist .grid .row-4, .m-itemlist .icon-has-more { overflow: visible !important}');
-    GM_addStyle(".m-itemlist .grid .icon { margin-top: 0px !important; }");
-  }
+    if (is_show_shop_pocket) {
+        GM_addStyle(
+            ".icon-fest-2017taobaodianpuhong { background: url(https://img.alicdn.com/tfs/TB1D5hoab_I8KJjy1XaXXbsxpXa-407-396.png);   background-repeat: no-repeat; display: inline-block; background-position: -151px -304px;  width: 70px;  height: 16px; -webkit-filter: hue-rotate(300deg); }"
+        );
+    }
+
+    if (is_change_label_color){
+        GM_addStyle(".icon-fest-jianianhuamanjian1{ -webkit-filter: hue-rotate(5deg); }");
+        GM_addStyle(".icon-fest-jianianhuamanjian2{ -webkit-filter: hue-rotate(20deg);}");
+        GM_addStyle(".icon-fest-jianianhuamanjian3{ -webkit-filter: hue-rotate(35deg);}");
+    }
+
+    if (is_show_all_1111_labels) {
+        // .item-ctx-hover
+        GM_addStyle(".m-itemlist .grid .item { height: 376px !important; margin-bottom: -16px }");
+        GM_addStyle(
+            ".response-narrow  .m-itemlist .grid .item { height: 336px !important}"
+        );
+        GM_addStyle(
+            ".response-wider  .m-itemlist .grid .item { height: 406px !important}"
+        );
+        GM_addStyle(
+            ".m-itemlist .grid .row-4 { margin-top: 1px !important; margin-bottom: 20px;}"
+        );
+        GM_addStyle('.m-itemlist .grid .row-4, .m-itemlist .icon-has-more { overflow: visible !important}');
+        GM_addStyle(".m-itemlist .grid .icon { margin-top: 0px !important; }");
+    }
 })();
